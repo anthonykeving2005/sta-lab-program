df482d398a42ceb3b44ad1305b6bf94f1f883422
Anthony Kevin <167439169+anthonykeving2005@users.noreply.github.com>
2026-06-22 05:45:11 +0530

    STA LAB PROGRAM
    STA LAB PROGRAM

---
+++ prog1.py
@@ -0,0 +1,11 @@
+a=int(input("Eneter a number:"))
+b=int(input("enter a number :"))
+add=a+b
+print("The sum of two numbers is:",add)
+sub=a-b
+print("The difference of two numbers is:",sub)
+mul=a*b
+print("The product of two numbers is:",mul)
+
+div=a/b
+print("The quotient of two numbers is:",div)
---
+++ prog3.py
@@ -0,0 +1,5 @@
+password=input("Enter a password:") 
+if 16<=len(password)<=18:
+    print("The password is valid")
+else:    
+    print("The password is invalid")
